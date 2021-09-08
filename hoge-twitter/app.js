d7b29f1517659b6f6fa240c5359dbc8919b65568
nkgw-program <nakagawa@example.com>
2021-09-08 12:23:44 +0900

    update: body-parser

--- hoge-twitter/app.js
+++ hoge-twitter/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const session = require('express-session');
 const app = express();
 const port = 3001;
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
